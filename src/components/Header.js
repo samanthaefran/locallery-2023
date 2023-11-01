26380895175245856b243a3cd35f2fab06bb4059
Samantha Elaine <85855171+ybsamantha@users.noreply.github.com>
2023-11-01 12:34:19 -0400

    changes to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
 function Header() {
-  return <h1> Locallery </h1>
+  return (
+    <>
+    <div>
+      <Header className="text-center">Locallery</Header>
+    </div>
+    <div className="grid justify-items-end"></div>
+    </>
+  )
 }
 
 export default Header
